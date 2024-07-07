1beff61ea9dbb86ca1b6b2700707bd24bb961771
mert-ucman <rati.be@hotmail.com>
2024-07-07 23:57:17 +0300

    ShopApp Eklendi

---
+++ GUN3ODEV3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Customer customer1 = new Customer
+{
+    Id = 1,
+    Name = "Mert",
+    Surname = "Uçman",
+    Number = "11111111111",
+    Adress = "Hatay",
+    Balance = 5000
+};
+
+
+Customer customer2 = new Customer
+{
+    Id = 2,
+    Name = "Arda",
+    Surname = "Uçman",
+    Number = "22222222222",
+    Adress = "Adana",
+    Balance = 8000
+};
+
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+customerManager.Delete(customer1);
+customerManager.Listing();
